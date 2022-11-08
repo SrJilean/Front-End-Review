fd1cea51043d5983eb4a961e4194b70fda11be54
Jilber Leandro Canchica Bautista <canchicaleandro@gmail.com>
2022-11-08 14:52:38 -0500

    Commit Final

--- src/app/muestra/muestra.module.ts
+++ src/app/muestra/muestra.module.ts
@@ -8,17 +8,22 @@ import { Hijo1Component } from './parametros/hijo1/hijo1.component';
 import { Hijo2Component } from './parametros/hijo2/hijo2.component';
 import { FormsModule } from '@angular/forms';
 
-
-
 @NgModule({
-  declarations: [DirectivasComponent,MainComponent,ParametrosComponent,
-  PipesComponent,
-  Hijo1Component,
-  Hijo2Component],
-  exports:[DirectivasComponent, MainComponent, ParametrosComponent, PipesComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-  ]
+  declarations: [
+    DirectivasComponent,
+    MainComponent,
+    ParametrosComponent,
+    PipesComponent,
+    Hijo1Component,
+    Hijo2Component,
+  ],
+  exports: [
+    DirectivasComponent,
+    MainComponent,
+    ParametrosComponent,
+    PipesComponent,
+  ],
+  imports: [CommonModule, FormsModule],
+  providers:[]
 })
-export class MuestraModule { }
+export class MuestraModule {}
